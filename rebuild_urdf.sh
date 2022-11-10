f9ed3dbeedd8f1fd84097c98386b914c72c6e358
Jørgen Myrvold <jorgeum@stud.ntnu.no>
2022-11-10 14:40:07 +0100

    Cleaned up. Created moveit config

--- rebuild_urdf.sh
+++ rebuild_urdf.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-rm -rf devel/ build/
+rm -rf devel/ build/ log/
 catkin_make
 source devel/setup.bash
 echo "CONVERTING URDF"
 rosrun xacro xacro -o "src/kmriiwa_description/urdf/robot/$1.urdf" "src/kmriiwa_description/urdf/robot/$1.urdf.xacro"
 echo "FINISHED CONVERTING"
+
